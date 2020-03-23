39f9ec9eb1148405f6171a96a0ba08727a6e12d2
Zoltan Etedi <zetedi@gmail.com>
2020-03-23 15:44:55 +0100

    maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent implements OnInit {
 
-  title = 'core';
   constructor() { }
-  ngOnInit() {
 
+  title = 'core';
+
+  ngOnInit() {
   }
 }
--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var ol: any;
+
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
+
+
 export class DetailsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  latitude: number = 50.8355;
+  longitude: number = 4.4035;
+
+  map: any;
+
+  ngOnInit() {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([this.longitude, this.latitude]),
+        zoom: 12
+      })
+    });
+    this.addPoint(this.latitude, this.longitude);
   }
 
+  setCenter() {
+    var view = this.map.getView();
+    view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
+    view.addMarker(ol.proj.fromLonLat([this.longitude, this.latitude]));
+    view.setZoom(12);
+  }
+
+  addPoint(lat: number, lng: number) {
+    var vectorLayer = new ol.layer.Vector({
+      source: new ol.source.Vector({
+        features: [new ol.Feature({
+          geometry: new ol.geom.Point(ol.proj.transform([lng, lat], 'EPSG:4326', 'EPSG:3857')),
+        })]
+      }),
+      style: new ol.style.Style({
+        image: new ol.style.Icon({
+          anchor: [0.5, 0.5],
+          anchorXUnits: "fraction",
+          anchorYUnits: "fraction",
+          src: "favicon.ico"
+        })
+      })
+    });
+    this.map.addLayer(vectorLayer);
+  }
 }
+
