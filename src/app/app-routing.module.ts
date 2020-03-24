38ac985f2ce86fc4d02e8cf1df869d8855040bf0
Zoltan Etedi <zetedi@gmail.com>
2020-03-24 19:09:30 +0100

    details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrdersComponent } from './orders/orders.component';
-import { DetailsComponent } from './details/details.component';
+import { MapComponent } from './map/map.component';
+import { LifetreeDetailsComponent } from './lifetrees/lifetree-details/lifetree-details.component';
 
 
 const routes: Routes = [
   { path: 'login', pathMatch: 'full', redirectTo: '/login'},
   { path: 'orders/:id', component: OrdersComponent },
-  { path: 'details', component: DetailsComponent },
+  { path: 'map', component: MapComponent },
+  { path: 'lifetree-details', component: LifetreeDetailsComponent },
   { path: '', pathMatch: 'full', redirectTo: '/lifetrees'},
   { path: '**', pathMatch: 'full', redirectTo: '/lifetrees' }
 ];
--- src/app/lifetrees/lifetrees.module.ts
+++ src/app/lifetrees/lifetrees.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule }      from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -8,11 +8,12 @@ import { LifetreesComponent } from './lifetrees.component';
 import { LifetreesListComponent } from './lifetrees-list/lifetrees-list.component';
 import { LifetreesFilterComponent } from './lifetrees-list/lifetrees-filter/lifetrees-filter.component';
 
-import {LifetreesRoutingModule} from './lifetrees-routing.module';
+import { LifetreesRoutingModule } from './lifetrees-routing.module';
+import { LifetreeDetailsComponent } from './lifetree-details/lifetree-details.component';
 
 @NgModule({
     imports: [CommonModule, SharedModule, FormsModule, LifetreesRoutingModule],
-    declarations: [LifetreesComponent, LifetreesListComponent, LifetreesFilterComponent],
+    declarations: [LifetreesComponent, LifetreesListComponent, LifetreesFilterComponent, LifetreeDetailsComponent],
     exports: [LifetreesComponent]
 })
 export class LifetreesModule { }
