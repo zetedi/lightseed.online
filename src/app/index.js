8d8ef2cd42c2ec1607c8cce3b736b9a484417316
Zoltan Etedi <zetedi@gmail.com>
2020-03-31 18:22:27 +0200

    Responsive navigation

--- src/app/index.js
+++
@@ -1,9 +0,0 @@
-let width = document.getElementById('width');
-var onresize = function() {
-   width.innerText = document.body.clientWidth;
-   width.classList.add('display-width');
-   setTimeout(() => {
-       width.classList.remove('display-width');
-   }, 2000)
-}
-window.addEventListener("resize", onresize);
