f4dcf7db599d9310bc9edb41073a4342212d18a2
Zoltan Etedi <zetedi@gmail.com>
2020-08-17 14:39:56 +0200

    Add files via upload

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  lights: [
+    {
+      lifeseedHandle: "lifeseed",
+      body: "this is the light body",
+      createdAt: "2020-08-12T10:36:55.378Z",
+      seeCount: 49,
+      reflectCount: 7,
+    },
+  ],
+};
