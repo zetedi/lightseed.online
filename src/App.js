496a59792878062acc7fc5c3f2e0b63bb10bdbeb
zetedi <zetedi@gmail.com>
2020-08-26 22:05:12 +0200

    Lights

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import { ThemeProvider } from "@material-ui/styles";
+import { purple } from "@material-ui/core/colors";
+//Pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+//Components
+import Navbar from "./components/Navbar";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: purple[500],
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: "#11cb5f",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
+import Light from "../components/Light";
+
+export class home extends Component {
+  state = {
+    lights: null,
+  };
+  componentDidMount() {
+    axios
+      .get("/lights")
+      .then((res) => {
+        this.setState({
+          lights: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+  render() {
+    let recentLightsMarkup = this.state.lights ? (
+      this.state.lights.map((light) => <Light light={light} />)
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={10}>
+        <Grid item sm={8} xs={12}>
+          {recentLightsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default home;
