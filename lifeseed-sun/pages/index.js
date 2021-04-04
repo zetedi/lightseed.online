cce91dfc75db249f39def5bba4e8d6b8a17058f7
Zoltan Etedi <zetedi@gmail.com>
2021-04-05 00:41:20 +0200

    3JS splash

--- lifeseed-sun/pages/index.js
+++ lifeseed-sun/pages/index.js
@@ -1,1 +1,87 @@
-export { default } from './products';
+import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ReactDOM from 'react-dom';
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import DatGui, {
+  DatBoolean,
+  DatColor,
+  DatNumber,
+  DatString,
+} from 'react-dat-gui';
+
+const useStyles = makeStyles((theme) => ({
+  ...theme.customTheme,
+  landing: {
+    display: 'grid',
+  },
+}));
+
+function Sphere(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+  // Return view, these are regular threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <sphereGeometry args={[1, 37, 37]} />
+      <meshStandardMaterial color={hovered ? 'violet' : 'orange'} />
+    </mesh>
+  );
+}
+
+function Torus(props) {
+  const mesh = useRef();
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+  return (
+    <mesh {...props} ref={mesh} scale={1}>
+      <torusGeometry args={[10, 3, 16, 100]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
+
+export default function Index() {
+  const classes = useStyles();
+
+  //   const [state, setState] = useState({
+  //     data: {
+  //       package: 'react-dat-gui',
+  //       power: 9000,
+  //       isAwesome: true,
+  //       feelsLike: '#2FA1D6',
+  //     },
+  //   });
+  //   const handleUpdate = (newData) =>
+  //     setState((prevState) => ({
+  //       data: { ...prevState.data, ...newData },
+  //     }));
+
+  return (
+    <Box className={classes.landing}>
+      <Canvas style={{ height: '70vh' }}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Sphere position={[0, 0, 0]} />
+        <Torus position={[0, 0, 0]} />
+      </Canvas>
+      {/* <DatGui data={state.data} onUpdate={handleUpdate}>
+        <DatString path="package" label="Package" />
+        <DatNumber path="power" label="Power" min={9000} max={9999} step={1} />
+        <DatBoolean path="isAwesome" label="Awesome?" />
+        <DatColor path="feelsLike" label="Feels Like" />
+      </DatGui> */}
+    </Box>
+  );
+}
