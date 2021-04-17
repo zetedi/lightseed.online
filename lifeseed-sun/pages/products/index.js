ba6716b1615d604b7e224ff4b4737427ff31b8de
Zoltan Etedi <zetedi@gmail.com>
2021-04-17 23:34:42 +0200

    Product index.

--- lifeseed-sun/pages/products/index.js
+++ lifeseed-sun/pages/products/index.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/dist/client/router';
+import { Box } from '@material-ui/core';
 import Products from '../../components/Products';
 import Pagination from '../../components/Pagination';
 
@@ -6,10 +7,10 @@ export default function ProductsPage() {
   const { query } = useRouter();
   const page = parseInt(query.page);
   return (
-    <div>
+    <Box style={{ paddingRight: '.7rem' }}>
       <Pagination page={page || 1} />
       <Products page={page || 1} />
       <Pagination page={page || 1} />
-    </div>
+    </Box>
   );
 }
