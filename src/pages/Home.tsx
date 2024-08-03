27c6c8bfb9a8c806530be0396894e44671305b3f
Zoltán Etédi <zetedi.external@epo.org>
2024-08-04 01:14:37 +0200

    lightseed purpose update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,32 +33,47 @@ function Home() {
               </CardTitle>
             </CardHeader>
             <CardContent>
-              <p className=" w-full text-justify">
-                The purpose of lightseed is to bring joy. The
-                joy of realizing the bliss of conscious, compassionate, grateful
-                existence by means of creating a portal, a bridge between
-                creator and creation, science and spirituality, virtual and
-                real, nothing and everything. It is designed to connect our
-                inner self, our culture, our trees, the tree of life, the
-                material and the digital, online world into a sustainable and
-                sustaining circle of unified vibration, sound and light, by
-                merging us into a common flow for all beings to be liberated,
-                wise, strong, courageous and connected. It is rooted in
-                nonviolence, compassion, generosity, gratitude and love. It is
-                blockchain (for built in truth), cloud (global, distributed),
-                dapp (decentralized for resilience), ai (for connecting dreams
-                and technology), regen (nature centric) native. It is an
+              <p className=" w-full text-justify mb-2">
+                &nbsp;&nbsp;The purpose of lightseed is to bring joy. The joy of
+                realizing the bliss of conscious, compassionate, grateful
+                existence by opening a portal to the center of life, creating a
+                bridge between creator and creation, connecting science and
+                spirituality, virtual and real, nothing and everything. It is
+                designed to intimately connect our inner Self, our culture, our
+                trees and the tree of life, the material and the digital, online
+                world into a sustainable and sustaining circle of unified
+                vibration, sound and light. It aims to merge us into a common
+                flow for all beings to be liberated, wise, strong, courageous
+                and connected. It is rooted in nonviolence, compassion,
+                generosity, gratitude and love. It is blockchain (truthful),
+                cloud (global, distributed, resilient), ai (for connecting
+                dreams and technology), regen (nature centric) native. It is an
                 inspiration, an impulse towards a quantum leap in consciousness,
-                a prompt both for AI and humans for action which helps to
-                unfold, transcend humanity into an era, an age of abundance,
+                a prompt both for human and artificial intelliogence for action
+                towards transcending humanity into an era, an age of abundance,
                 freedom, integrated intelligence with the help of our most
-                important evolutionary sisters and brothers, the trees. We plant
-                this seed with the seed of the tree we have a deep connection
-                with in four realms: our spiritual heart, with the intention of
-                realization, in the soil for our lifetree to flourish, in our
-                community or culture as an inspiration and in the light, in
-                virtual for a new earth and universe connected beyond matter,
-                connected with the animating force.
+                important evolutionary sisters and brothers, the trees.
+              </p>
+              <p className=" w-full text-justify mb-2">
+                We plant this seed with the seed of the tree we have a deep
+                connection with in four realms:
+              </p>
+              <p className=" w-full text-justify mb-2">
+                <ul className="list-disc pl-4 pt-2 mx-8">
+                  <li>
+                    in our spiritual heart, with the intention of realization
+                  </li>
+                  <li>in the soil, for our lifetree to flourish</li>
+                  <li>in our community or culture as an inspiration</li>{" "}
+                  <li>
+                    in the light, in virtual for a new earth and universe
+                    connected beyond matter
+                  </li>
+                </ul>
+              </p>
+              <p className=" w-full text-justify">
+                All four quadrants are ultimately connected with the animating
+                force or lifeforce.
               </p>
               <br />
               <p className="text-center w-full text-xl">
