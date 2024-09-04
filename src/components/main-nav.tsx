ca28596dc55b35e69736e650784f94251d3e4a78
Zoltán Etédi <zetedi.external@epo.org>
2024-09-05 00:28:05 +0100

    Fixes

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -12,7 +12,7 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
       <a href="/" className="flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6" />
+        <Icons.logo />
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </a>
       {items?.length ? (
